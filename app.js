d8706f58a185d113bd9005264e707e109e166ced
Seonho <dkdlatjsh@gmail.com>
2021-11-19 03:08:53 +0900

    const, let and var

--- app.js
+++ app.js
@@ -1,12 +1,19 @@
-// variable practice
-const a = 5;
+// const and let
+const a = 5; // const : 변경 불가
 const b = 2; // != "2"
 
-const myName = "seonho"; //변수 이름 내  공백x
-const veryLongVariableName = 0; //java (camel case)
-const very_long_variable_name = 0; //python (snake_case)
+let myName = "seonho"; // let : 변경 가능
+
+var old = 'old version'; // 의도치 않게 값이 
+                        //  변경되는 것을 막기위해 const let 사용
 
 console.log(a + b);
 console.log(a * b);
 console.log(a / b);
 console.log("hello" + myName);
+
+// a = 10 ; error
+
+myName = "sh";
+
+console.log('your new name is '+ myName);
