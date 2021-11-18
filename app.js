0b7342cb842dc4cc6e0ef33ab743e669b4361d73
Seonho <dkdlatjsh@gmail.com>
2021-11-19 02:38:10 +0900

    variable practice

--- app.js
+++ app.js
@@ -1,6 +1,12 @@
 // variable practice
 const a = 5;
+const b = 2; // != "2"
 
-console.log(a + 2);
-console.log(a * 2);
-console.log(a / 2);
+const myName = "seonho"; //변수 이름 내  공백x
+const veryLongVariableName = 0; //java (camel case)
+const very_long_variable_name = 0; //python (snake_case)
+
+console.log(a + b);
+console.log(a * b);
+console.log(a / b);
+console.log("hello" + myName);
