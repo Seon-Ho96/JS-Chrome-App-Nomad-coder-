a49b3493dc06b652885a02947ec32eaf34bfc098
Seonho <dkdlatjsh@gmail.com>
2021-11-19 14:46:57 +0900

    simple event handler 구현

--- app.js
+++ app.js
@@ -1,45 +1,42 @@
 
+const title = document.querySelector("div.hello:first-child h1");
 
+console.dir(title);
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-//function
-function plus(firstnumber, secondnumber){
-    console.log(firstnumber + secondnumber);
-}
-function divide(a, b){
-    console.log(a / b);
-}
-
-plus(8, 60);
-divide(50, 4);
-
-const player = {
-    name : 'seonho',
-    sayHello : function(otherPersonsName){
-        console.log("hello " + otherPersonsName + " nice to meet you");
+const superEventHandler ={
+    handleTitleClick: function() {
+        title.style.color = "blue";
+    },
+    handleTitleRightClick: function() {
+        title.innerText = "right button is clicked!";
+    },
+    handleMouseEnter: function(){
+        title.innerText = "Mouse is here!";
+    },
+    handleMouseLeave: function(){
+        title.innerText = "Mouse is gone!";
+    },
+    handleWindowResize: function(){
+        title.innerText = "You just resized!";
+        document.body.style.backgroundColor = "tomato";
+    },
+    handleWindowCopy: function(){
+        alert("copier!!");
+    },
+    handleWindowOffline: function(){
+        alert("SOS no WIFI");
+    },
+    handleWindowOnline: function(){
+        alert("ALL Good!");
     }
 }
+title.addEventListener("click", superEventHandler.handleTitleClick);
+title.addEventListener("contextmenu", superEventHandler.handleTitleRightClick);
+title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
+title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
 
-console.log(player.name);
-player.sayHello("seo");
+
+window.addEventListener("resize", superEventHandler.handleWindowResize);
+window.addEventListener("copy", superEventHandler.handleWindowCopy);
+window.addEventListener("offline", superEventHandler.handleWindowOffline);
+window.addEventListener("online", superEventHandler.handleWindowOnline);
