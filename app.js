a43066fdd01d43e4c5f018bd1b2db3c7acc4f807
Seonho <dkdlatjsh@gmail.com>
2021-11-19 15:17:26 +0900

    return

--- app.js
+++ app.js
@@ -1,42 +1,23 @@
+const calculator = {
+    plus: function(a,b) {
+        return(a+b);
+    },
+    minus: function(a,b) {
+        return(a-b);
+    },
+    times: function(a,b) {
+        return(a*b);
+    },
+    divide: function(a,b) {
+        return(a/b);
+    },
+    power: function(a,b) {
+        return(a**b);
+    },
+};
 
-const title = document.querySelector("div.hello:first-child h1");
-
-console.dir(title);
-
-const superEventHandler ={
-    handleTitleClick: function() {
-        title.style.color = "blue";
-    },
-    handleTitleRightClick: function() {
-        title.innerText = "right button is clicked!";
-    },
-    handleMouseEnter: function(){
-        title.innerText = "Mouse is here!";
-    },
-    handleMouseLeave: function(){
-        title.innerText = "Mouse is gone!";
-    },
-    handleWindowResize: function(){
-        title.innerText = "You just resized!";
-        document.body.style.backgroundColor = "tomato";
-    },
-    handleWindowCopy: function(){
-        alert("copier!!");
-    },
-    handleWindowOffline: function(){
-        alert("SOS no WIFI");
-    },
-    handleWindowOnline: function(){
-        alert("ALL Good!");
-    }
-}
-title.addEventListener("click", superEventHandler.handleTitleClick);
-title.addEventListener("contextmenu", superEventHandler.handleTitleRightClick);
-title.addEventListener("mouseenter", superEventHandler.handleMouseEnter);
-title.addEventListener("mouseleave", superEventHandler.handleMouseLeave);
-
-
-window.addEventListener("resize", superEventHandler.handleWindowResize);
-window.addEventListener("copy", superEventHandler.handleWindowCopy);
-window.addEventListener("offline", superEventHandler.handleWindowOffline);
-window.addEventListener("online", superEventHandler.handleWindowOnline);
+const plusResult = calculator.plus(2,3);
+const minusResult = calculator.minus(plusResult,10);
+const timesResult = calculator.times(10,minusResult);
+const divideResult = calculator.divide(timesResult,plusResult);
+const powerResult = calculator.power(divideResult,minusResult);
