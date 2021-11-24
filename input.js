c56aa781840e1be462c737b7dcb58132ca9fffda
Seonh0 <dkdlatjsh@gmail.com>
2021-11-25 03:09:34 +0900

    localStorage

--- input.js
+++ input.js
@@ -9,6 +9,7 @@ const greeting = document.querySelector("#greeting");
 const link = document.querySelector("a");
 
 const HIDDEEN_CLASSNAME = "hidden"; //관습적으로 string으로만 이루어진 변수는 대문자로 표기
+const USERNAME_KEY = "username";
 
 function onLoginSubmit(event) {
     //function의 첫번째 argument는 방금 일어난 것(event)에 대한 정보를 가지고 있다.
@@ -17,9 +18,9 @@ function onLoginSubmit(event) {
     //value값은 input에 입력한 값 (기본값 설정 가능 - html에서)
     loginForm.classList.add(HIDDEEN_CLASSNAME);
     const username = loginInput.value;
+    localStorage.setItem(USERNAME_KEY,username);
     // greeting.innerText = "Hello " + username;
-    greeting.innerText = `Hello ${username}`;
-    greeting.classList.remove(HIDDEEN_CLASSNAME);
+    paintGreetings(username);
     // if(username === ""){
     //     alert("Please write your name");
     // }else if(username.length > 15){
@@ -27,10 +28,25 @@ function onLoginSubmit(event) {
     // }
 }
 
+function paintGreetings(username){
+    greeting.innerText = `Hello ${username}`;
+    greeting.classList.remove(HIDDEEN_CLASSNAME);
+}
+
 function handleLinkClick(event){
     event.preventDefault()
     // alert("Click!"); 링크이동 막음(ok누르면 링크 이동)
 }
 //fdd
-loginForm.addEventListener("submit", onLoginSubmit);
+
 link.addEventListener("click", handleLinkClick);
+
+const savedUsername = localStorage.getItem(USERNAME_KEY);
+
+if(savedUsername === null){
+    //show the form
+    loginForm.classList.remove(HIDDEEN_CLASSNAME);
+    loginForm.addEventListener("submit", onLoginSubmit);
+} else{
+    paintGreetings(savedUsername);
+}
